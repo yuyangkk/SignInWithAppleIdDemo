97a5830fa8937a917855c1373d255a949e8669f5
kk <wanan_0124@163.com>
2019-12-23 11:10:12 +0800

    Initial Commit

---
+++ SignInWithAppleDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignInWithAppleDemo
+//
+//  Created by Kaye on 2019/12/23.
+//  Copyright © 2019 Kaye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
